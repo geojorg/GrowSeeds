47eab4c37296e6983586a9e8b6cb757609bc5151
Jorge Guerrero Montes <geojorg@gmail.com>
2019-11-24 10:15:32 -0500

    Update Startup.cs

--- GrowSeeds.Web/Startup.cs
+++ GrowSeeds.Web/Startup.cs
@@ -42,7 +42,6 @@ namespace GrowSeeds.Web
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
